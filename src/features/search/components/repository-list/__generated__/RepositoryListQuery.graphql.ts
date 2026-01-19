dcd8a16342a87971ee48f2ccbef517a0aac8b304
한송담 <hansongdam@hansongdam-ui-MacBookPro.local>
2026-01-19 22:50:01 +0900

    chunk: add RepositoryListQuery graphql file

--- src/features/search/components/repository-list/__generated__/RepositoryListQuery.graphql.ts
+++ src/features/search/components/repository-list/__generated__/RepositoryListQuery.graphql.ts
@@ -8,23 +8,17 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { ConcreteRequest, FragmentRefs } from 'relay-runtime';
+import { ConcreteRequest } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
 export type RepositoryListQuery$variables = {
   searchQuery: string;
 };
 export type RepositoryListQuery$data = {
   readonly search: {
-    readonly nodes:
-      | ReadonlyArray<
-          | {
-              readonly id?: string;
-              readonly ' $fragmentSpreads': FragmentRefs<'RepositoryItem_repository'>;
-            }
-          | null
-          | undefined
-        >
-      | null
-      | undefined;
+    readonly nodes: ReadonlyArray<{
+      readonly id?: string;
+      readonly " $fragmentSpreads": FragmentRefs<"RepositoryItem_repository">;
+    } | null | undefined> | null | undefined;
   };
 };
 export type RepositoryListQuery = {
@@ -32,258 +26,263 @@ export type RepositoryListQuery = {
   variables: RepositoryListQuery$variables;
 };
 
-const node: ConcreteRequest = (function () {
-  var v0 = [
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "searchQuery"
+  }
+],
+v1 = [
+  {
+    "kind": "Literal",
+    "name": "first",
+    "value": 10
+  },
+  {
+    "kind": "Variable",
+    "name": "query",
+    "variableName": "searchQuery"
+  },
+  {
+    "kind": "Literal",
+    "name": "type",
+    "value": "REPOSITORY"
+  }
+],
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "RepositoryListQuery",
+    "selections": [
       {
-        defaultValue: null,
-        kind: 'LocalArgument',
-        name: 'searchQuery',
-      },
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "SearchResultItemConnection",
+        "kind": "LinkedField",
+        "name": "search",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": null,
+            "kind": "LinkedField",
+            "name": "nodes",
+            "plural": true,
+            "selections": [
+              {
+                "kind": "InlineFragment",
+                "selections": [
+                  (v2/*: any*/),
+                  {
+                    "args": null,
+                    "kind": "FragmentSpread",
+                    "name": "RepositoryItem_repository"
+                  }
+                ],
+                "type": "Repository",
+                "abstractKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
     ],
-    v1 = [
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "RepositoryListQuery",
+    "selections": [
       {
-        kind: 'Literal',
-        name: 'first',
-        value: 10,
-      },
-      {
-        kind: 'Variable',
-        name: 'query',
-        variableName: 'searchQuery',
-      },
-      {
-        kind: 'Literal',
-        name: 'type',
-        value: 'REPOSITORY',
-      },
-    ],
-    v2 = {
-      alias: null,
-      args: null,
-      kind: 'ScalarField',
-      name: 'id',
-      storageKey: null,
-    },
-    v3 = {
-      alias: null,
-      args: null,
-      kind: 'ScalarField',
-      name: 'name',
-      storageKey: null,
-    };
-  return {
-    fragment: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: 'Fragment',
-      metadata: null,
-      name: 'RepositoryListQuery',
-      selections: [
-        {
-          alias: null,
-          args: v1 /*: any*/,
-          concreteType: 'SearchResultItemConnection',
-          kind: 'LinkedField',
-          name: 'search',
-          plural: false,
-          selections: [
-            {
-              alias: null,
-              args: null,
-              concreteType: null,
-              kind: 'LinkedField',
-              name: 'nodes',
-              plural: true,
-              selections: [
-                {
-                  kind: 'InlineFragment',
-                  selections: [
-                    v2 /*: any*/,
-                    {
-                      args: null,
-                      kind: 'FragmentSpread',
-                      name: 'RepositoryItem_repository',
-                    },
-                  ],
-                  type: 'Repository',
-                  abstractKey: null,
-                },
-              ],
-              storageKey: null,
-            },
-          ],
-          storageKey: null,
-        },
-      ],
-      type: 'Query',
-      abstractKey: null,
-    },
-    kind: 'Request',
-    operation: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: 'Operation',
-      name: 'RepositoryListQuery',
-      selections: [
-        {
-          alias: null,
-          args: v1 /*: any*/,
-          concreteType: 'SearchResultItemConnection',
-          kind: 'LinkedField',
-          name: 'search',
-          plural: false,
-          selections: [
-            {
-              alias: null,
-              args: null,
-              concreteType: null,
-              kind: 'LinkedField',
-              name: 'nodes',
-              plural: true,
-              selections: [
-                {
-                  alias: null,
-                  args: null,
-                  kind: 'ScalarField',
-                  name: '__typename',
-                  storageKey: null,
-                },
-                {
-                  kind: 'InlineFragment',
-                  selections: [
-                    v2 /*: any*/,
-                    v3 /*: any*/,
-                    {
-                      alias: null,
-                      args: null,
-                      kind: 'ScalarField',
-                      name: 'description',
-                      storageKey: null,
-                    },
-                    {
-                      alias: null,
-                      args: null,
-                      kind: 'ScalarField',
-                      name: 'stargazerCount',
-                      storageKey: null,
-                    },
-                    {
-                      alias: null,
-                      args: null,
-                      kind: 'ScalarField',
-                      name: 'forkCount',
-                      storageKey: null,
-                    },
-                    {
-                      alias: null,
-                      args: null,
-                      kind: 'ScalarField',
-                      name: 'updatedAt',
-                      storageKey: null,
-                    },
-                    {
-                      alias: null,
-                      args: [
-                        {
-                          kind: 'Literal',
-                          name: 'first',
-                          value: 1,
-                        },
-                      ],
-                      concreteType: 'LanguageConnection',
-                      kind: 'LinkedField',
-                      name: 'languages',
-                      plural: false,
-                      selections: [
-                        {
-                          alias: null,
-                          args: null,
-                          concreteType: 'LanguageEdge',
-                          kind: 'LinkedField',
-                          name: 'edges',
-                          plural: true,
-                          selections: [
-                            {
-                              alias: null,
-                              args: null,
-                              concreteType: 'Language',
-                              kind: 'LinkedField',
-                              name: 'node',
-                              plural: false,
-                              selections: [v3 /*: any*/, v2 /*: any*/],
-                              storageKey: null,
-                            },
-                          ],
-                          storageKey: null,
-                        },
-                      ],
-                      storageKey: 'languages(first:1)',
-                    },
-                    {
-                      alias: null,
-                      args: null,
-                      concreteType: 'License',
-                      kind: 'LinkedField',
-                      name: 'licenseInfo',
-                      plural: false,
-                      selections: [
-                        {
-                          alias: null,
-                          args: null,
-                          kind: 'ScalarField',
-                          name: 'spdxId',
-                          storageKey: null,
-                        },
-                        v3 /*: any*/,
-                        v2 /*: any*/,
-                      ],
-                      storageKey: null,
-                    },
-                    {
-                      alias: null,
-                      args: null,
-                      concreteType: 'IssueConnection',
-                      kind: 'LinkedField',
-                      name: 'issues',
-                      plural: false,
-                      selections: [
-                        {
-                          alias: null,
-                          args: null,
-                          kind: 'ScalarField',
-                          name: 'totalCount',
-                          storageKey: null,
-                        },
-                      ],
-                      storageKey: null,
-                    },
-                  ],
-                  type: 'Repository',
-                  abstractKey: null,
-                },
-                {
-                  kind: 'InlineFragment',
-                  selections: [v2 /*: any*/],
-                  type: 'Node',
-                  abstractKey: '__isNode',
-                },
-              ],
-              storageKey: null,
-            },
-          ],
-          storageKey: null,
-        },
-      ],
-    },
-    params: {
-      cacheID: '239fdb5c57d78a8ce3b0b69ccf3ea6c5',
-      id: null,
-      metadata: {},
-      name: 'RepositoryListQuery',
-      operationKind: 'query',
-      text: 'query RepositoryListQuery(\n  $searchQuery: String!\n) {\n  search(query: $searchQuery, type: REPOSITORY, first: 10) {\n    nodes {\n      __typename\n      ... on Repository {\n        id\n        ...RepositoryItem_repository\n      }\n      ... on Node {\n        __isNode: __typename\n        id\n      }\n    }\n  }\n}\n\nfragment RepositoryItem_repository on Repository {\n  id\n  name\n  description\n  stargazerCount\n  forkCount\n  updatedAt\n  languages(first: 1) {\n    edges {\n      node {\n        name\n        id\n      }\n    }\n  }\n  licenseInfo {\n    spdxId\n    name\n    id\n  }\n  issues {\n    totalCount\n  }\n}\n',
-    },
-  };
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "SearchResultItemConnection",
+        "kind": "LinkedField",
+        "name": "search",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": null,
+            "kind": "LinkedField",
+            "name": "nodes",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "__typename",
+                "storageKey": null
+              },
+              {
+                "kind": "InlineFragment",
+                "selections": [
+                  (v2/*: any*/),
+                  (v3/*: any*/),
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "description",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "stargazerCount",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "forkCount",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "updatedAt",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": [
+                      {
+                        "kind": "Literal",
+                        "name": "first",
+                        "value": 1
+                      }
+                    ],
+                    "concreteType": "LanguageConnection",
+                    "kind": "LinkedField",
+                    "name": "languages",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "LanguageEdge",
+                        "kind": "LinkedField",
+                        "name": "edges",
+                        "plural": true,
+                        "selections": [
+                          {
+                            "alias": null,
+                            "args": null,
+                            "concreteType": "Language",
+                            "kind": "LinkedField",
+                            "name": "node",
+                            "plural": false,
+                            "selections": [
+                              (v3/*: any*/),
+                              (v2/*: any*/)
+                            ],
+                            "storageKey": null
+                          }
+                        ],
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": "languages(first:1)"
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "License",
+                    "kind": "LinkedField",
+                    "name": "licenseInfo",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "spdxId",
+                        "storageKey": null
+                      },
+                      (v3/*: any*/),
+                      (v2/*: any*/)
+                    ],
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "IssueConnection",
+                    "kind": "LinkedField",
+                    "name": "issues",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "totalCount",
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "type": "Repository",
+                "abstractKey": null
+              },
+              {
+                "kind": "InlineFragment",
+                "selections": [
+                  (v2/*: any*/)
+                ],
+                "type": "Node",
+                "abstractKey": "__isNode"
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "239fdb5c57d78a8ce3b0b69ccf3ea6c5",
+    "id": null,
+    "metadata": {},
+    "name": "RepositoryListQuery",
+    "operationKind": "query",
+    "text": "query RepositoryListQuery(\n  $searchQuery: String!\n) {\n  search(query: $searchQuery, type: REPOSITORY, first: 10) {\n    nodes {\n      __typename\n      ... on Repository {\n        id\n        ...RepositoryItem_repository\n      }\n      ... on Node {\n        __isNode: __typename\n        id\n      }\n    }\n  }\n}\n\nfragment RepositoryItem_repository on Repository {\n  id\n  name\n  description\n  stargazerCount\n  forkCount\n  updatedAt\n  languages(first: 1) {\n    edges {\n      node {\n        name\n        id\n      }\n    }\n  }\n  licenseInfo {\n    spdxId\n    name\n    id\n  }\n  issues {\n    totalCount\n  }\n}\n"
+  }
+};
 })();
 
-(node as any).hash = '2869a7e4cf5f43d5146f955732a85985';
+(node as any).hash = "2869a7e4cf5f43d5146f955732a85985";
 
 export default node;
